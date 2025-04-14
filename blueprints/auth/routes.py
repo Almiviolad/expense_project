8f7ecf708c8f707fa2387f07c053e9fa7071fb72
Aloh Michael <mykelaloh@gmail.com>
2025-04-14 14:26:44 +0100

    add token refresh to login

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -4,14 +4,14 @@ from flask import request, jsonify
 import models
 from models import storage
 from models.user import User
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, get_jwt_identity, jwt_required
 
 
-@auth_bp.route('/login', methods={'POST'})
+@auth_bp.route('/login', methods=['POST'])
 def login():
     """sign the user in andgenerate sesion JWT token"""
     data = request.get_json()
-    if 'email' and 'password' in data:
+    if 'email' in data and 'password' in data:
         email = data['email']
         password = data['password']
     else:
@@ -20,9 +20,25 @@ def login():
     if not user or not user.verify_password(password):
         return jsonify({'error': 'Invalid credentials'}), 400
     access_token = create_access_token(identity=user.id)
-    return jsonify({'access_token': access_token}), 201
+    refresh_token = create_refresh_token(identity=user.id)
+    response = jsonify({'message': 'Logged in successfully'})
+    
+    set_access_cookies(response, access_token)
+    set_refresh_cookies(response, refresh_token)
+    return response, 200
+    
+  
+@auth_bp.route('/refresh', methods=['GET'])
+@jwt_required(refresh=True)
+def refresh_token():
+  user_id = get_jwt_identity()
+  access_token = create_access_token(identity= user_id)
+  response = jsonify({'message': 'Token refreshed successfully'})
+  set_access_cookies(response, access_token)
+  return response, 200
 
-@auth_bp.route('/signup', methods={'POST'})
+
+@auth_bp.route('/signup', methods=['POST'])
 def signup():
     """register new user"""
     data = request.get_json()
@@ -38,8 +54,8 @@ def signup():
     try:
         #Try to add new user with the details
         new_user = User(username=data['username'], password=data['password'], email=data['email'], monthly_income=data['monthly_income'], currency=data['currency'])
-    except KeyError as err:
-        return jsonify({'error': str(err)})    
-    storage.add(new_user)
-    storage.save()
+        storage.add(new_user)
+        storage.save()
+    except Exception as err:
+        return jsonify({'error': str(err)})
     return jsonify({'message': 'Registration successful. You can now login with your details'}), 201
--- config.py
+++ config.py
@@ -3,8 +3,14 @@ import datetime
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT Configs
+    JWT_TOKEN_LOCATION = ['cookies']
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=1)  # 1 day
-    JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(weeks=1)
+    JWT_ACCESS_COOKIE_PATH = '/'
+    JWT_REFRESH_COOKIE_PATH = '/auth/refresh'
+    JWT_COOKIE_CSRF_PROTECT = False 
+    JWT_COOKIE_SECURE = False
+    JWT_SESSION_COOKIE = False
+    JWT_ACCESS_TOKEN_EXPIRES = 60 * 60  # 1 hour
+    JWT_REFRESH_TOKEN_EXPIRES = 30 * 24 * 60 * 60  # 30 days
